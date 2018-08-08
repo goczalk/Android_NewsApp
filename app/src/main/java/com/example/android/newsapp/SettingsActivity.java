a371fc9172a0a4138a9b65d6943b83dd68540f32
Klaudia Goczal <klaudia@lodz.pl>
2018-08-08 14:14:32 +0200

    Added menu option and empty settings fragment

---
+++ app/src/main/java/com/example/android/newsapp/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.newsapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by klaudia on 08/08/18.
+ */
+
+public class SettingsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class NewsPreferenceFragment extends PreferenceFragment{
+
+    }
+}
